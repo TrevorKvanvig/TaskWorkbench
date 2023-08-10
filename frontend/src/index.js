89659c4dd73811952dfdbef227e546fedbcb4ad3
TrevorKvanvig <tkvanvig10@gmail.com>
2023-08-09 17:38:43 -0700

    Authentication Constext Provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BoardsContextProvider } from './contexts/BoardsContext';
+import { AuthContextProvider } from './contexts/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BoardsContextProvider>
-      <App />
-    </BoardsContextProvider>
+    <AuthContextProvider>
+      <BoardsContextProvider>
+        <App />
+      </BoardsContextProvider>    
+    </AuthContextProvider>
+    
   </React.StrictMode>
 );
