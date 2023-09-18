cb48df6d93f91f98f5d8083f83f501718f90b879
TrevorKvanvig <tkvanvig10@gmail.com>
2023-09-18 16:14:47 -0700

    disable DevTools For production

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import { BoardsContextProvider } from './contexts/BoardsContext';
 import { AuthContextProvider } from './contexts/AuthContext';
+import { disableReactDevToosl } from '@fvilers/disable-react-devtools';
+
+disableReactDevToosl();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
