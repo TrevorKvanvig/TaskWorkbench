0301fb7650c7e9a5d9aa9c4a0193fe24dc572484
TrevorKvanvig <tkvanvig10@gmail.com>
2023-09-25 21:14:46 -0700

    Ready For Deployment

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,6 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function(req,res) {
-		res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 
 // allows req.body to be used
