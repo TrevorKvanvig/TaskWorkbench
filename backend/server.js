b61151251b744c07f55fe0720ee1a3d9a0acd243
TrevorKvanvig <tkvanvig10@gmail.com>
2023-09-25 21:02:53 -0700

    route debugging

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,14 @@ const cors = require('cors');
 //============== initialize App =========
 const app = express();
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req,res) {
+		res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 // allows req.body to be used
 app.use(express.json());
 
